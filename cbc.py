252c8acd8a7419237f97f8105bdd99d8d3159f8f
MrRazorez <viantosa1999@gmail.com>
2022-05-26 21:53:10 +0700

    Pening, Paklsls

--- cbc.py
+++ cbc.py
@@ -1,6 +1,98 @@
+from re import I
+from gen_bin import hek_conv
 from keystream import *
 
 ambilKey = keystream()
 
-class cbc :
-    pass
+def __empatbit(teks) :
+    if len(teks) < 4 :
+        tp = ""
+        for i in range(4-len(teks)) :
+            tp += "0"
+
+        teks = tp+teks
+
+    return teks
+
+
+def __plainHex(teks) :
+    temp = ""
+    for i in teks :
+        temp += gen_bin.hek_conv(ord(i))
+
+    return temp
+
+def plainASCII(teks) :
+    i = 1
+    temp = ""
+
+    while i < len(teks) :
+        tmp = teks[i-1] + teks[i]
+        tmp = gen_bin.hek_conv(tmp)
+        temp += chr(tmp)
+        i += 2
+
+    return temp
+
+def __wrap_left(karakter) :
+    tmp = gen_bin.bin_conv(gen_bin.hek_conv(karakter))
+    if len(tmp) < 4 :
+        tp = ""
+        for i in range(4-len(tmp)) :
+            tp += "0"
+
+        tmp = tp+tmp
+
+    tmp = tmp[1:]+tmp[0]
+
+    return gen_bin.hek_conv(gen_bin.bin_conv(tmp))
+
+def __wrap_right(karakter) :
+    tmp = gen_bin.bin_conv(gen_bin.hek_conv(karakter))
+    if len(tmp) < 4 :
+        tp = ""
+        for i in range(4-len(tmp)) :
+            tp += "0"
+
+        tmp = tp+tmp
+
+    tmp = tmp[len(tmp)-1]+tmp[:len(tmp)-1]
+    
+    return gen_bin.hek_conv(gen_bin.bin_conv(tmp))
+
+def enkripsi(plain, K, IV) :
+    plain_hex = __plainHex(plain)
+    print(plain_hex)
+    temp = ""
+
+    i = 0
+    for j in plain_hex :
+        tmp = gen_bin.hek_conv(gen_bin.hek_conv(j) ^ gen_bin.hek_conv(IV))
+        tmp = __wrap_left(gen_bin.hek_conv(gen_bin.hek_conv(tmp) ^ gen_bin.hek_conv(K[i])))
+        temp += tmp
+        IV = tmp
+
+        i += 1
+        if i == len(K) :
+            i %= len(K)
+    print(temp)
+
+    return temp
+
+def dekripsi(chiper, K, IV) :
+    print(chiper)
+    temp = ""
+
+    i = 0
+    for j in chiper :
+        tmp = gen_bin.hek_conv(gen_bin.hek_conv(__wrap_right(j)) ^ gen_bin.hek_conv(K[i]))
+        tmp = gen_bin.hek_conv(gen_bin.hek_conv(tmp) ^ gen_bin.hek_conv(IV))
+        temp += tmp
+        IV = j
+
+        i += 1
+        if i == len(K) :
+            i %= len(K)
+    print(temp)
+
+    return temp
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-import keystream
+import gen_bin
+import cbc
+
+def ngeTEST(teks) :
+    i = 1
+    temp = ""
+
+    while i < len(teks) :
+        tmp = teks[i-1] + teks[i]
+        tmp = gen_bin.hek_conv(tmp)
+        temp += chr(tmp)
+        i += 2
+
+    return temp
 
 if __name__ == "__main__" :
-    coba = keystream.keystream()
-    print(coba.lfsr)
-    print(coba.U)
-    print(coba.IV)
+    teks = "hello"
+    print(teks)
+    coba = cbc.enkripsi(teks, cbc.ambilKey.lfsr, cbc.ambilKey.IV)
+    hasil = cbc.dekripsi(coba, cbc.ambilKey.lfsr, cbc.ambilKey.IV)
+    print(cbc.plainASCII(hasil))
